dd680cd3e44f0d2c467b29079a6b47eba158c581
Rupok1 <rupokhasan789@gmail.com>
2022-07-25 16:18:44 +0600

    Update project

--- LMS.sql
+++ LMS.sql
@@ -6,7 +6,7 @@ DROP TABLE ADMIN;
 DROP TABLE LOGIN;
 
 
----CREATING TABLE---
+---CREATING TABLE---//LAB-01
 
 CREATE TABLE LOGIN(LOGIN_ID NUMBER(15),
                    PASSWORD VARCHAR(10),
@@ -16,8 +16,9 @@ CREATE TABLE LOGIN(LOGIN_ID NUMBER(15),
 CREATE TABLE ADMIN(ADMIN_ID NUMBER(15) NOT NULL,
                     NAME VARCHAR(50),
                     PHONE_NO NUMBER(15),
-                    PRIMARY KEY(ADMIN_ID),
-                    FOREIGN KEY (ADMIN_ID) REFERENCES LOGIN(LOGIN_ID));
+                    LOGIN_ID NUMBER(15),
+                    PRIMARY KEY(ADMIN_ID), 
+                    FOREIGN KEY (LOGIN_ID) REFERENCES LOGIN(LOGIN_ID));
 
 CREATE TABLE USERS(LIBRARY_ID NUMBER(15) NOT NULL,
                   NAME VARCHAR(50),
@@ -25,8 +26,9 @@ CREATE TABLE USERS(LIBRARY_ID NUMBER(15) NOT NULL,
                   ADDRESS VARCHAR(100),
                   NO_OF_BOOK_ISSUED NUMBER(5),
                   ISSUED_FROM NUMBER(15),
+                  LOGIN_ID NUMBER(15),
                   PRIMARY KEY(LIBRARY_ID),
-                  FOREIGN KEY(LIBRARY_ID) REFERENCES LOGIN(LOGIN_ID),
+                  FOREIGN KEY(LOGIN_ID) REFERENCES LOGIN(LOGIN_ID),
                   FOREIGN KEY(ISSUED_FROM) REFERENCES ADMIN(ADMIN_ID)
                   );
 
@@ -39,24 +41,24 @@ CREATE TABLE BOOK(BOOK_BARCODE_NO VARCHAR(20) NOT NULL,
                   FOREIGN KEY (BORROWED_BY) REFERENCES USERS(LIBRARY_ID),
                   FOREIGN KEY (ISSUE_BY) REFERENCES ADMIN(ADMIN_ID)
                   );
-CREATE TABLE ISSUED_BY(BOOK_ID VARCHAR(20) NOT NULL,
-                     ADMIN_ID NUMBER(15) NOT NULL,
+CREATE TABLE ISSUED_BY(ISSUE_ID NUMBER(20) NOT NULL,
+                     BOOK_ID VARCHAR(20),
+                     ADMIN_ID NUMBER(15),
                      ISSUE_DATE VARCHAR(10),
                      FINE NUMBER(5),
                      RETURN_DATE VARCHAR(10),
-                     PRIMARY KEY(BOOK_ID,ADMIN_ID),
+                     PRIMARY KEY(ISSUE_ID),
                      FOREIGN KEY (BOOK_ID) REFERENCES BOOK(BOOK_BARCODE_NO),
-                     FOREIGN KEY(ADMIN_ID) REFERENCES ADMIN(ADMIN_ID)
+                     FOREIGN KEY (ADMIN_ID) REFERENCES ADMIN(ADMIN_ID)
                      );
 CREATE TABLE BOOK_RECORD(BOOK_ID VARCHAR(20) NOT NULL,
-                        BOOK_TITE VARCHAR(250),
+                        BOOK_TITLE VARCHAR(250),
                         BOOK_AUTHOR VARCHAR(250),
                         BOOK_STATUS VARCHAR(50),
                         BOOK_ENTRY_DATE VARCHAR(50),
-                        ADMIN_ID NUMBER(15) NOT NULL,
+                        ADMIN_ID NUMBER(15),
                         PRIMARY KEY(BOOK_ID),
-                        FOREIGN KEY(ADMIN_ID) REFERENCES ADMIN(ADMIN_ID)
+                        FOREIGN KEY(ADMIN_ID) REFERENCES ADMIN(ADMIN_ID) on delete cascade
                          );
-
 
 
